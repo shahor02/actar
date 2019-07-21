c8e02fcb8f01648ccf2f476dd1a0d5c5aca654e7
shahoian <ruben.shahoyan@cern.ch>
2019-07-21 20:15:24 +0200

    xx

--- actar.C
+++ actar.C
@@ -1,9 +1,10 @@
 #include <TChain.h>
 #include <vector>
 
-constexpr float PitchX = 0.03, PitchY = 0.05, PitchZ = 0.03;  
+constexpr int MinPointsPerEvent = 3; // reject event if N points too small
 
 struct Point {
+  static constexpr float PitchX = 0.03, PitchY = 0.05, PitchZ = 0.03;  
   //Declaration of leaves types
   Int_t           PixelNb;
   Int_t           TrackID;
@@ -15,16 +16,81 @@ struct Point {
   Double_t        PixelCentreYPosition;
   Double_t        PixelCentreZPosition;
   Int_t           BigSensorNb;
+  void print() const {
+    int ix(0),iy(0),iz(0);
+    xyz2cell(ix,iy,iz);
+    printf("Ev:%4d XYZ:{%+.3e,%+.3e,%+.3e}={%4d/%4d/%4d} Tr:%3d PDG:%+4d Sn:%2d Ed:%.2e BigSn:%2d Pix:%d\n",
+	   EventID,PixelCentreXPosition,PixelCentreYPosition,PixelCentreZPosition,ix,iy,iz,TrackID,PartPDGCode,
+	   SensorNb,Edep,BigSensorNb,PixelNb);
+  }
+  
+  void xyz2cell(int &ix, int &iy, int &iz) const {
+    ix = PixelCentreXPosition/PitchX;
+    iy = PixelCentreYPosition/PitchY;
+    iz = PixelCentreZPosition/PitchZ;
+  }
+
+  int compare(const Point& pntB) const {
+    // ordering in X,Y,Z
+    if (PixelCentreXPosition < pntB.PixelCentreXPosition) return -1;
+    if (PixelCentreXPosition > pntB.PixelCentreXPosition) return  1;
+    if (PixelCentreYPosition < pntB.PixelCentreYPosition) return -1;
+    if (PixelCentreYPosition > pntB.PixelCentreYPosition) return  1;
+    if (PixelCentreZPosition < pntB.PixelCentreZPosition) return -1;
+    if (PixelCentreZPosition > pntB.PixelCentreZPosition) return  1;
+    return 0;
+  }
+
+  bool isNeighbour(const Point& pntB) const {
+    // check if points are neighbours (difference in pixels <=1 in all dimensions
+    if (std::abs(PixelCentreXPosition - pntB.PixelCentreXPosition)>1.5*PitchX ||
+	std::abs(PixelCentreYPosition - pntB.PixelCentreYPosition)>1.5*PitchY ||
+	std::abs(PixelCentreZPosition - pntB.PixelCentreXPosition)>1.5*PitchZ) {
+      return false;
+    }
+    else {
+      return true;
+    }
+      
+  }
+  
   ClassDefNV(Point,1);
 };
+
 
 TChain inpTree;
 Point point, *pointPtr = &point;
 Long64_t entID = -1;
 Long64_t currEventID = -1;
 
-std::vector<Point> points;
-std::vector<int> pointsIdx;
+std::vector<Point> pointsPool;
+std::vector<int> pointsPoolIdx;
+
+
+
+struct chain
+{
+  int status = 0;
+  std::vector<int> pointIDs; // set of connected poits
+
+  chain(int pntID) {
+    status = 0;
+    pointIDs.push_back(pntID);
+  }
+
+  const Point& getGlobalPoint(int i) const {
+    return pointsPool[ pointsPoolIdx[i] ];
+  }
+  
+  bool isNeighbourToLast(const Point& pnt) const {
+    if (!pointIDs.size()) return false; // should not happen
+    const auto& last = getGlobalPoint(pointIDs.back());
+    return last.isNeighbour(pnt);
+  }
+  
+};
+std::vector<chain> chains;
+
 
 int initTree(const char* nameFile = "B2_Digit_5000ev.root", const char* nameTree = "Interest")
 {
@@ -45,9 +111,10 @@ int initTree(const char* nameFile = "B2_Digit_5000ev.root", const char* nameTree
   return inpTree.GetEntries();
 }
 
+
 int getNextEvent()
 {
-  points.clear();
+  pointsPool.clear();
   bool first = true;
   if (entID>=inpTree.GetEntries()) {
     return -1;
@@ -59,30 +126,45 @@ int getNextEvent()
       first = false;
     }
     if (point.EventID!=currEventID) break;
-    points.push_back(point);
+    pointsPool.push_back(point);
   }
-  printf("read %d points for event %lld\n", (int)points.size(), currEventID);
-  return points.size();
+  printf("read %d points for event %lld\n", (int)pointsPool.size(), currEventID);
+  return pointsPool.size();
 }
 
 void ProcessEvent()
 {
   // sort points in X (beam direction) then Y then Z
-  pointsIdx.clear();
-  pointsIdx.resize(points.size());
-  for (int i=points.size();i--;) pointsIdx[i] = i;
-  std::sort(pointsIdx.begin(), pointsIdx.end(), [](int a, int b) {
-      const auto& pntA = points[a];
-      const auto& pntB = points[b];
-      
-      if (pntA.PixelCentreXPosition == pntB.PixelCentreXPosition) {
-	if (pntA.PixelCentreYPosition == pntB.PixelCentreYPosition) {
-	  return pntA.PixelCentreZPosition < pntB.PixelCentreZPosition;
-	}
-	return pntA.PixelCentreYPosition < pntB.PixelCentreYPosition;
-      }
-      return pntA.PixelCentreXPosition < pntB.PixelCentreXPosition;
+  int npnt = pointsPool.size();
+
+  if (npnt<MinPointsPerEvent) return;
+  pointsPoolIdx.clear();
+  pointsPoolIdx.resize(npnt);
+  for (int i=npnt;i--;) pointsPoolIdx[i] = i;
+  std::sort(pointsPoolIdx.begin(), pointsPoolIdx.end(), [](int a, int b) {
+      int res = pointsPool[a].compare( pointsPool[b] );
+      return res<0;
     });
+  //
+  for (int ipt=0;ipt<npnt;ipt++) {
+    const auto& pnt = pointsPool[ pointsPoolIdx[ipt] ];
+    printf("%3d |",ipt);
+    pnt.print();
+  }
+
+  
+  // create 1st seed
+  chains.emplace_back(0);
+  
+  for (int ipt=1;ipt<npnt;ipt++) {
+    const auto& pnt = pointsPool[ pointsPoolIdx[ipt] ];
+
+    // skip repeating points
+    if (pnt.compare( pointsPool[ pointsPoolIdx[ipt] ] )==0) continue;
+    // loop over all chains and check if the point is compatible
+
+  }
+  
 }
 
 void actar(const char* nameFile = "B2_Digit_5000ev.root", const char* nameTree = "Interest")
